34a226df181b93c18ad6defaabde5291fc2ca547
jin1210 <jinj86530@gmail.com>
2022-09-26 14:10:02 +0000

    [update]

--- app/views/homes/app.js
+++ app/views/homes/app.js
@@ -81,6 +81,33 @@ function getJShand(){
   let js_hand_num = Math.floor( Math.random() * 3 );
   if(js_hand_num == 0){
     js_hand = "グー";
-  } else if(js_hand_num == 1)
+  } else if(js_hand_num == 1){
+    js_hand = "チョキ";
+  } else if(js_hand_num == 2){
+    js_hand = "パー";
+  }
+  
+  return js_hand;
+}
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
+
+let js_hand = getJShand();
+
+let judge = winLose(user_hand, js_hand);
+
+alert('あなたの選んだ手は' + user_hand + 'です。￥nJavaScriptの選んだ手は' + js_hand + 'です。￥n結果は' + judge + 'です。');
+
+function winLose(user, js){
+  let winLoseStr;
+  
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
   }
 }
